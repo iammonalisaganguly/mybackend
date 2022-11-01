248c50d130b9cefabb5b68547a405ca29f6ec601
monalisa <monalisaganguly620@gmail.com>
2022-11-01 20:58:35 +0530

    1st

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,46 +1,6 @@
-const UserModel= require("../models/userModel")
-
-
-
-
-const basicCode= async function(req, res) {
-    let tokenDataInHeaders= req.headers.token
-    console.log(tokenDataInHeaders)
-
-    console.log( "HEADER DATA ABOVE")
-    console.log( "hey man, congrats you have reached the Handler")
-    res.send({ msg: "This is coming from controller (handler)"})
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
+const basicCode = async function (req,res){
+    console.log(req.url)
+    console.log("hii i am path of this url ", req.path)
+    res.send({msg: "this is coming from controller handeler"})
 }
-
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
-}
-
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
-module.exports.basicCode= basicCode
+module.exports.basicCode=basicCode
